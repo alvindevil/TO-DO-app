896dcc21a8a756013a4f4ff7fb20c9905e28aa15
alvin devils <alvindevil568@gmail.com>
2023-11-02 17:49:08 +0530

    fixed buttons with li

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ document.addEventListener("DOMContentLoaded", function ()
 {
     const taskinput = document.getElementById("taskinput");
     const addtaskButton = document.getElementById("addtask");
-    const tasklist = document.getElementById("tasklist");
-    const complete_task = document.getElementById("complete-task");
-    const delete_task = document.getElementById("delete-task");
-    const buttons = document.getElementById("buttons");
+    const tasklist = document.getElementById("tasklist");    
+    const buttonsgroup = document.getElementById("buttons-group");
+    
 
     // Event listener to add a new task
     addtaskButton.addEventListener("click", function () 
@@ -13,31 +12,30 @@ document.addEventListener("DOMContentLoaded", function ()
         const tasktext = taskinput.value.trim();
         if (tasktext !== "") 
         {
+            const listbox = document.createElement("div");
             const listitem = document.createElement("li");
-            const done = document.createElement("button");
-            const deleteb = document.createElement("button");
+            const c_btn = document.createElement("button");
+            const d_btn = document.createElement("button");
+            const controlbuttons = document.createElement("div");
             listitem.innerHTML = `
-                <span>${tasktext}</span>
+                <p>${tasktext}</p>
             `;
-            tasklist.appendChild(listitem); 
-            buttons.appendChild(done);  
-            buttons.appendChild(deleteb); 
+            listbox.className = "listbox";
+            listbox.id = "listbox";
+            c_btn.innerHTML="done";
+            d_btn.innerHTML="delete";
+            tasklist.appendChild(listbox); 
+            listbox.append(listitem); 
+            listbox.append(c_btn);
+            listbox.append(d_btn);
             listitem.id = "listitem";
-            done.innerHTML = "Done";
-            done.id = "done-button";
-            deleteb.innerHTML="Delete"; 
-            deleteb.id = "delete-button";
-            controlicons.innerHTML = `
-            <div class="buttons-group">
-                <button id="complete-task"> <img src="🦆 icon _tick circle_.svg"></button>
-                <button id="delete-task"> <img src="🦆 icon _trash_.svg"></button>
-            </div>
-            `;
-            icons.append(controlicons);
             taskinput.value = "";
         }
     });
-    done = document.getElementById("done-button");
+
+
+    
+    /* done = document.getElementById("done-button");
     done.addEventListener("click", function()
     {
         listitem = document.getElementById("listitem");
@@ -45,11 +43,12 @@ document.addEventListener("DOMContentLoaded", function ()
     });
 
     // Event delegation for marking tasks as complete and deleting tasks
-    tasklist.addEventListener("click", function (e) {
+    tasklist.addEventListener("click", function (e) 
+    {
         if (e.target.classlist.contains("complete")) {
             e.target.parentElement.classList.toggle("completed");
         } else if (e.target.classList.contains("delete")) {
             e.target.parentElement.remove();
         }
-    });
+    }); */
 });
